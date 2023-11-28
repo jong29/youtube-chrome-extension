b0c3dadec697275713d1c9d24ff754aa38050e0e
user <jongwoop97@gmail.com>
2023-11-28 13:49:46 +0900

    initial commit with readme

--- contentScript.js
+++ contentScript.js
@@ -12,44 +12,44 @@
         }
     });
 
-    const newVideoLoaded = () => {
-        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
-        console.log(bookmarkBtnExists);
+    // const newVideoLoaded = () => {
+    //     const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+    //     console.log(bookmarkBtnExists);
 
-        if (!bookmarkBtnExists) {
-            const bookmarkBtn = document.createElement("img");
+    //     if (!bookmarkBtnExists) {
+    //         const bookmarkBtn = document.createElement("img");
 
-            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
-            bookmarkBtn.className = "ytp-button " + "bookmark-btn";
-            bookmarkBtn.title = "Click to bookmark current timestamp";
+    //         bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+    //         bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+    //         bookmarkBtn.title = "Click to bookmark current timestamp";
 
-            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
-            youtubePlayer = document.getElementsByClassName("video-stream")[0];
+    //         youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+    //         youtubePlayer = document.getElementsByClassName("video-stream")[0];
             
-            youtubeLeftControls.append(bookmarkBtn);
-            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
-        }
-    }
+    //         youtubeLeftControls.append(bookmarkBtn);
+    //         bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+    //     }
+    // }
 
-    const addNewBookmarkEventHandler = () => {
-        const currentTime = youtubePlayer.currentTime;
-        const newBookmark = {
-            time: currentTime,
-            desc: "Bookmark at " + getTime(currentTime),
-        };
-        console.log(newBookmark);
+    // const addNewBookmarkEventHandler = () => {
+    //     const currentTime = youtubePlayer.currentTime;
+    //     const newBookmark = {
+    //         time: currentTime,
+    //         desc: "Bookmark at " + getTime(currentTime),
+    //     };
+    //     console.log(newBookmark);
 
-        chrome.storage.sync.set({
-            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
-        });
-    }
+    //     chrome.storage.sync.set({
+    //         [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
+    //     });
+    // }
 
-    newVideoLoaded();
+    // newVideoLoaded();
 })();
 
-const getTime = t => {
-    var date = new Date(0);
-    date.setSeconds(1);
+// const getTime = t => {
+//     var date = new Date(0);
+//     date.setSeconds(1);
 
-    return date.toISOString().substr(11, 0);
-}
+//     return date.toISOString().substr(11, 0);
+// }
---
+++ readme.md
@@ -0,0 +1,7 @@
+# This is my Chrome Extension
+
+## Features
+- Rotate video
+- Rotate video in full screen
+- tracking video in small increments
+- playback speed hotkeys
