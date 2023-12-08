5820aded47c59aeb5f2b5c949cc802ca047ddc51
user <jongwoop97@gmail.com>
2023-12-08 16:42:18 +0900

    tutorial ing...

--- contentScript.js
+++ contentScript.js
@@ -12,24 +12,26 @@
         }
     });
 
-    // const newVideoLoaded = () => {
-    //     const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
-    //     console.log(bookmarkBtnExists);
+    const newVideoLoaded = () => {
+        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+        console.log(bookmarkBtnExists);
+        
+        if (!bookmarkBtnExists) {
+            const bookmarkBtn = document.createElement("img");
 
-    //     if (!bookmarkBtnExists) {
-    //         const bookmarkBtn = document.createElement("img");
+            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+            bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+            bookmarkBtn.title = "Click to bookmark current timestamp";
 
-    //         bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
-    //         bookmarkBtn.className = "ytp-button " + "bookmark-btn";
-    //         bookmarkBtn.title = "Click to bookmark current timestamp";
-
-    //         youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
-    //         youtubePlayer = document.getElementsByClassName("video-stream")[0];
+            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+            youtubePlayer = document.getElementsByClassName("video-stream")[0];
             
-    //         youtubeLeftControls.append(bookmarkBtn);
-    //         bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
-    //     }
-    // }
+            youtubeLeftControls.append(bookmarkBtn);
+            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+        }
+        
+        console.log(bookmarkBtnExists);
+    }
 
     // const addNewBookmarkEventHandler = () => {
     //     const currentTime = youtubePlayer.currentTime;
