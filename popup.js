4d7546bb6cb57f8133233f353de44ef915f95553
user <jongwoop97@gmail.com>
2023-12-15 22:57:57 +0900

    implement extension dynamic ui

--- popup.js
+++ popup.js
@@ -1,24 +1,37 @@
 import { getActiveTabURL } from "./utils.js";
 
 // adding a new bookmark row to the popup
-const addNewBookmark = () => {};
-
-const viewBookmarks = () => {};
-
-const onPlay = e => {};
-
-const onDelete = e => {};
-
-const setBookmarkAttributes =  () => {};
-
 document.addEventListener("DOMContentLoaded", async () => {
-    const activeTab = await getActiveTabURL();
-    const queryParameters = activeTab.url.split("?")[1];
-    const urlParams = new URLSearchParams(queryParameters);
+  const activeTab = await getActiveTabURL();
+  const queryParameters = activeTab.url.split("?")[1];
+  const urlParams = new URLSearchParams(queryParameters);
 
-    const currentVideo = urlParams.get("v");
-    
-    if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
-        chrome.storage.sync.get([])
-    }
+  const currentVideo = urlParams.get("v");
+
+  if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
+    chrome.storage.sync.get([]);
+  }
+});
+
+document.getElementById("active").addEventListener("change", (e) => {
+  console.log(e);
+  if (e.target.checked) {
+    console.log("checked");
+    const timer = document.querySelector(".timer");
+    console.log(timer);
+    timer.innerHTML = `
+       <div>
+       time to watch: <input id="time" type='text' /> <br>
+       !!!!!time to watch: <input id="time2" type='time' /> <br>
+       watch one video: <input id="one-vid" type='checkbox' /> <br>
+       <button id="start-btn">Start</button>
+       </div>
+       `;
+
+       const timeInput = document.getElementById("time");
+       console.log(timeInput);
+    // add event listeners to each input type
+  } else {
+    console.log("unchecked");
+  }
 });
