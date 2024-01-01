aa77eb04ef45aec3972dc7995742a85de6b414d3
박종우 <jongwoop97@gmail.com>
2024-01-01 14:57:01 +0900

    implementing local storage state

--- popup.js
+++ popup.js
@@ -1,28 +1,12 @@
 import { getActiveTabURL } from "./utils.js";
 
-const endState = {activated : false, watchTime : 0, oneVideo: false};
+const endState = { activated: false, watchTime: 0, oneVideo: false };
 
-// adding a new bookmark row to the popup
-document.addEventListener("DOMContentLoaded", async () => {
-  const activeTab = await getActiveTabURL();
-  const queryParameters = activeTab.url.split("?")[1];
-  const urlParams = new URLSearchParams(queryParameters);
-
-  const currentVideo = urlParams.get("v");
-
-  if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
-    chrome.storage.sync.get([]);
-  } else {
-    const container = document.getElementById("container");
-    container.innerHTML = "<div>This is not a youtube video</div>";
-  }
-});
-
-document.getElementById("active").addEventListener("change", (e) => {
+const renderForm = () => {
+  console.log("debug");
   const timer = document.querySelector(".timer");
-  if (e.target.checked) {
-    endState.activated = true;
-    timer.innerHTML = `
+  endState.activated = true;
+  timer.innerHTML = `
       <div>
       watch one video: <input id="oneVid" type='checkbox' /> <br>
       time to watch: <input id="time" type="number" /> <br>
@@ -30,18 +14,39 @@ document.getElementById("active").addEventListener("change", (e) => {
       </div>
     `;
 
-    // add event listeners to each input type
-    document.getElementById("setBtn").addEventListener("click", (e) => {
-      endState.watchTime = document.getElementById("time").value;
-    });
+  // add event listeners to each input type
+  document.getElementById("setBtn").addEventListener("click", (e) => {
+    endState.watchTime = document.getElementById("time").value;
+  });
 
-    document.getElementById("oneVid").addEventListener("change", (e) => {
-      endState.activated = e.target.checked;
-    });
+  document.getElementById("oneVid").addEventListener("change", (e) => {
+    endState.activated = e.target.checked;
+  });
+};
 
+// adding a new bookmark row to the popup
+document.addEventListener("DOMContentLoaded", async () => {
+  const activeTab = await getActiveTabURL();
+  
+  const storageActive = chrome.storage.local.get("activated");
+  console.log();
+
+  if (!activeTab.url.includes("youtube.com/watch")) {
+    const container = document.getElementById("container");
+    container.innerHTML = "<div>This is not a youtube video</div>";
+  } else if (chrome.storage.local.get(["activated"]).then(isActive => isActive)) {
+    renderForm();
+  }
+});
+
+document.getElementById("active").addEventListener("change", (e) => {
+  const timer = document.querySelector(".timer");
+  chrome.storage.local.set({ activated: true });
+  if (e.target.checked) {
+    renderForm();
   } else {
     console.log("unchecked");
-    timer.innerHTML = '';
+    timer.innerHTML = "";
     endState.activated = false;
- }
+  }
 });
