580cb7211638aaf162c355cd8ccac8ed8ec9ffc7
박종우 <jongwoop97@gmail.com>
2024-01-03 21:27:20 +0900

    display timer on set click

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ const renderForm = () => {
   const timer = document.querySelector(".timer");
   endState.activated = true;
   timer.innerHTML = `
-      <div>
+      <div id = "formContainer">
       watch one video: <input id="oneVid" type='checkbox' /> <br>
       time to watch: <input id="time" type="number" /> <br>
       <button id="setBtn">SET</button>
@@ -16,6 +16,11 @@ const renderForm = () => {
   // add event listeners to each input type
   document.getElementById("setBtn").addEventListener("click", (e) => {
     endState.watchTime = document.getElementById("time").value;
+
+    const timerElement = document.createElement("div");
+    const formContainer = document.getElementById("formContainer");
+    timerElement.textContent = "00:00:00";
+    formContainer.appendChild(timerElement);
   });
 
   document.getElementById("oneVid").addEventListener("change", (e) => {
