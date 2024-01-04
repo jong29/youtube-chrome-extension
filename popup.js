90bba75090439a69c5d91a3b0f5c960de76e85ee
박종우 <jongwoop97@gmail.com>
2024-01-04 21:09:01 +0900

    divide watch time input into hours and minutes

--- popup.js
+++ popup.js
@@ -6,13 +6,14 @@ const endState = { activated: false, watchTime: 0, oneVideo: false };
 const renderForm = () => {
   const timer = document.querySelector(".timer");
   endState.activated = true;
-  timer.innerHTML = `
+  let innerForm = `
       <div id = "formContainer">
       watch one video: <input id="oneVid" type='checkbox' /> <br>
-      time to watch: <input id="time" type="number" /> <br>
-      <button id="setBtn">SET</button>
-      </div>
-    `;
+      time to watch : <input id = "hours" type = "number" min = "0" max = "24"/> hours  
+        <input id = "minutes" type = "number" min = "0" max = "59" /> minutes <br>
+        <button id="setBtn">SET</button>
+      </div>`;
+    timer.innerHTML = innerForm;
 
   // add event listeners to each input type
   document.getElementById("setBtn").addEventListener("click", (e) => {
