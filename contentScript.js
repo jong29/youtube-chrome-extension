2f592956809e357acfdaf9e2bc5aba24071bb787
jongwoo <jongwoop97@gmail.com>
2024-02-02 17:54:55 +0900

    set watchhtime to 0 after timeout, and format to 2 indents

--- contentScript.js
+++ contentScript.js
@@ -2,57 +2,63 @@
 // need to add a listener to when the timer is changed
 
 (() => {
-    let youtubeLeftControls, youtubePlayer;
-    let currentVideo = "";
-    let currentVideoBookmarks = [];
+  let youtubeLeftControls, youtubePlayer;
+  let currentVideo = "";
+  let currentVideoBookmarks = [];
 
-    // get watch time (mycode)
-    const fetchWatchTime = () => {
-        return new Promise((resolve) => {
-            chrome.storage.local.get(["watchTimeSeconds"], (obj) => {
-                resolve(obj["watchTimeSeconds"] ? JSON.parse(obj["watchTimeSeconds"]) : 0);
-            });
-        });
-    };
-    // end of my code
-
-
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { type, value, videoId } = obj;
-
-        if (type === "NEW") {
-            currentVideo = videoId;
-        }
+  // get watch time (mycode)
+  const fetchWatchTime = () => {
+    return new Promise((resolve) => {
+      chrome.storage.local.get(["watchTimeSeconds"], (obj) => {
+        resolve(
+          obj["watchTimeSeconds"] ? JSON.parse(obj["watchTimeSeconds"]) : 0
+        );
+      });
     });
+  };
+  // end of my code
 
-    const newVideoLoaded = async () => {
-        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
-        console.log(currentWatchTime, "current watch time");
-        setTimeout (() => {
-            alert("stop watching bro");
-        }, currentWatchTime * 1000);
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { type, value, videoId } = obj;
 
-        // add bookmark button to youtube video bookmark button
-        if (!bookmarkBtnExists) {
-            const bookmarkBtn = document.createElement("img");
+    if (type === "NEW") {
+      currentVideo = videoId;
+    }
+  });
 
-            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
-            bookmarkBtn.className = "ytp-button " + "bookmark-btn";
-            bookmarkBtn.title = "Click to bookmark current timestamp";
-
-            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
-            youtubePlayer = document.getElementsByClassName("video-stream")[0];
-            
-            youtubeLeftControls.append(bookmarkBtn);
-        }
+  const newVideoLoaded = async () => {
+    const bookmarkBtnExists =
+      document.getElementsByClassName("bookmark-btn")[0];
+    console.log(currentWatchTime, "current watch time");
+    if (currentWatchTime > 0) {
+      setTimeout(() => {
+        alert("stop watching bro");
+        chrome.storage.local.set({ watchTimeSeconds: 0 });
+      }, currentWatchTime * 1000);
     }
 
-    newVideoLoaded();
+    // add bookmark button to youtube video bookmark button
+    if (!bookmarkBtnExists) {
+      const bookmarkBtn = document.createElement("img");
+
+      bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+      bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+      bookmarkBtn.title = "Click to bookmark current timestamp";
+
+      youtubeLeftControls =
+        document.getElementsByClassName("ytp-left-controls")[0];
+      youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+      youtubeLeftControls.append(bookmarkBtn);
+    }
+  };
+
+  newVideoLoaded();
 })();
 
-const getTime = t => {
-    var date = new Date(0);
-    date.setSeconds(1);
+const getTime = (t) => {
+  var date = new Date(0);
+  date.setSeconds(1);
 
-    return date.toISOString().substr(11, 0);
-}
+  return date.toISOString().substr(11, 0);
+};
