32ddbea762a51df93af390c6ff4647e21511cb7f
박종우 <jongwoop97@gmail.com>
2024-06-09 18:38:32 +0900

    wip: separate content script for covering screen

--- contentScript.js
+++ contentScript.js
@@ -4,49 +4,9 @@
 (() => {
   let youtubeLeftControls, youtubePlayer;
 
-  // get watch time (mycode)
-  const fetchWatchTime = async () => {
-    return new Promise((resolve) => {
-      chrome.storage.local.get(["watchTimeSeconds"], (obj) => {
-        resolve(
-          obj["watchTimeSeconds"] ? JSON.parse(obj["watchTimeSeconds"]) : 0
-        );
-      });
-    });
-  };
-
-  // add black screen to youtube page
-  const coverScreen = () => {
-    console.log("coverScreen");
-    const body = document.getElementsByTagName("body")[0];
-    const bodyFirstChild = body.firstChild;
-    const blackScreen = document.createElement("div");
-    blackScreen.style.position = "fixed";
-    blackScreen.style.width = "100%";
-    blackScreen.style.height = "100%";
-    blackScreen.style.backgroundColor = "black";
-    blackScreen.style.zIndex = "9999";
-    blackScreen.style.top = "0";
-    blackScreen.style.left = "0";
-    blackScreen.textContent = "You have reached your watch time limit";
-    body.insertBefore(blackScreen, bodyFirstChild);
-  }
-
   const newVideoLoaded = async () => {
     const bookmarkBtnExists =
       document.getElementsByClassName("bookmark-btn")[0];
-
-    const currentWatchTime = await fetchWatchTime();
-
-    console.log("currentWatchTime", currentWatchTime);
-
-    if (currentWatchTime > 0) {
-      setTimeout(() => {
-        // insert black screen
-        coverScreen();
-        chrome.storage.local.set({ watchTimeSeconds: 0 });
-      }, currentWatchTime * 1000);
-    }
 
     // add bookmark button to youtube video bookmark button
     if (!bookmarkBtnExists) {
@@ -64,15 +24,11 @@
     }
   };
 
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log("message received in content script", message);
-  });
   newVideoLoaded();
 })();
 
-const getTime = (t) => {
-  var date = new Date(0);
-  date.setSeconds(1);
-
-  return date.toISOString().substr(11, 0);
-};
+// const getTime = (t) => {
+//   var date = new Date(0);
+//   date.setSeconds(1);
+//   return date.toISOString().substr(11, 0);
+// };
