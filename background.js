9396e60c74f55b268d49f613261020833fd5148f
Ryan Park <jongwoop97@gmail.com>
2024-08-18 17:57:28 +0900

    temp

--- background.js
+++ background.js
@@ -1,20 +1,22 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
 
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v"),
+    });
+  }
+});
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log("sending message", message);
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, { message: "Hello from Background!", timeToWatchSeconds: message.timeToWatchSeconds });
+    const activeTab = tabs[0];
+    console.log(activeTab);
+    chrome.tabs.sendMessage(activeTab.id, {
+      timeToWatchSeconds: message.timeToWatchSeconds,
+    });
   });
 });
